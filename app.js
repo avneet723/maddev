6eb075fa6ba958dcf0cc79cab1b14805f005ec1c
tms <tms@blackbox.(none)>
2013-01-29 18:43:01 -0500

    Removed Jade in favor of Swig,  & general cleanup of application

--- app.js
+++ app.js
@@ -2,27 +2,56 @@
  * Module dependencies.
  */
 var
-  express  = require('express'),
-  app      = express(),
-  routes   = require('./routes')(app), // Look in the "routes" directory
-  lessMW   = require('less-middleware'),
-  http     = require('http'),
-  path     = require('path');
+  // ExpressJS modules
+  express = require('express'),
+  app = express(),
+  http = require('http'),
+  path = require('path'),
+  // Swig Templates:
+  // http://paularmstrong.github.com/swig/
+  cons = require('consolidate'),
+  swig = require('swig'),
+  // Setup Dynamic routing, see:
+  // http://stackoverflow.com/questions/6059246/
+  routes = require('./routes')(app), // Look in the "routes" directory
+  // LESS Middelware
+  // https://github.com/emberfeather/less.js-middleware
+  lessMiddleware = require('less-middleware');
 
+/**
+ * Configuration
+ */
+// Swig templates
+swig.init({ root: __dirname + '/views', allowErrors: true });
+
+// Express JS
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3000); // port to listen on
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.engine('.html', cons.swig); // use Swig templates
+  app.set('view engine', 'html');
+
   app.use(express.bodyParser());
+  app.use(express.methodOverride());
   app.use(app.router);
-  app.use(lessMW({ src: __dirname + '/assets/css' }));
-  app.use("/assets", express.static(path.join(__dirname, 'assets')));
+
+  // LESS middleware
+  // https://npmjs.org/package/less-middleware
+  app.use(lessMiddleware({
+    src: __dirname + '/public',
+    compress: true
+  }));
+
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+/**
+ * Start The server(s)
+ */
 var server = http.createServer(app);
 
 server.listen(app.get('port'), function() {
