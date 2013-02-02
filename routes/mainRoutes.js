98ee896d5751ae86a52985d416302597514a7a9c
tms <tms@blackbox.(none)>
2013-02-02 12:21:43 -0500

    trying to figure out why azure is ignoring the data directory

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -11,12 +11,6 @@ module.exports = function(app) {
     });
   });
 
-  app.get('/maps', function(req, res){
-    res.render('maps', {
-      pageTitle: 'VT Maps!'
-    });
-  });
-
   app.get('/bus', function(req, res) {
     res.render('bus', {
       pageTitle: 'Bus Routes'
